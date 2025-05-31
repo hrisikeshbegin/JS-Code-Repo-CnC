f49cbefe29e453022134ee2125f1dd6bf15d9d82
hrisikeshbegin <hrisikesholt@gmail.com>
2025-05-31 06:41:33 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //this will be treated as newer version of JavaScript
+
+
+// alert(3+3)
+
+//we are using node JS not browser
+
+// console.log(3+3); console.log("hrisikesh")
+//bad readability
+
+//numer
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined =>
+//symbol => 
+
+//object
+
+console.log(typeof "Hrisikesh")
+
+console.log(typeof null)
+// type is object which can be called as a flaw in the JS language
+
+console.log(typeof undefined) //undefined
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 1453465
+let accountEmail = "a;shvaskjbvakjsd"
+var accountPassword = "346"
+
+accountEmail = "hrisikesholt@gmail.com"
+accountPassword = "24567"
+accountCity = "Bengaluru"
+
+
+
+//var is not used as it causes issueds with scope
+/*
+We use let only and var is used only in legacy code
+*/
+
+let accountState
+//variable not defined so automatically defined as undefined
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
+
